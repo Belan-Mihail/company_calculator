55b09e659d3833e089b8a71624e75bbb4a5fa3c8
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-17 13:26:15 +0100

    destruct product in ProductItem component

--- service-calculator-front/src/components/ProductItem.tsx
+++ service-calculator-front/src/components/ProductItem.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ProductItemProps } from '../types/Product'
 
 const ProductItem:React.FC<ProductItemProps> = ({product, onQuantityChange, onRemove}) => {
+  const {id, name, price, quantity, quantityInStock} = product
   return (
     <div>ProductItem</div>
   )
