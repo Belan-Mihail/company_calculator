f3380a3251aa0011a7ca402e6c398cbfc15ca1e5
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-17 13:30:05 +0100

    modify returned JSX in ProductItem component

--- service-calculator-front/src/components/ProductItem.tsx
+++ service-calculator-front/src/components/ProductItem.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import { ProductItemProps } from '../types/Product'
+import React from "react";
+import { ProductItemProps } from "../types/Product";
 
-const ProductItem:React.FC<ProductItemProps> = ({product, onQuantityChange, onRemove}) => {
-  const {id, name, price, quantity, quantityInStock} = product
+const ProductItem: React.FC<ProductItemProps> = ({
+  product,
+  onQuantityChange,
+  onRemove,
+}) => {
+  const { id, name, price, quantity, quantityInStock } = product;
   return (
-    <div>ProductItem</div>
-  )
-}
+    <div className="flex items-center space-x-4">
+      <span>
+        {name} {price}$
+      </span>
+      <input
+        type="range"
+        min="1"
+        max={quantityInStock}
+        value={quantity}
+        onChange={(e) => onQuantityChange(id, Number(e.target.value))}
+      />
+    </div>
+  );
+};
 
-export default ProductItem
+export default ProductItem;
