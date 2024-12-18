686f3ef23058fafccfc24866d22b9ac3640bfbfe
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-18 15:46:23 +0100

    fix bug: same id in all products

--- service-calculator-front/src/components/Calculator.tsx
+++ service-calculator-front/src/components/Calculator.tsx
@@ -10,9 +10,9 @@ import Total from './Total'
 
 const products: Product[] = [
   {id: 1, name: 'Product 1', price: 2.34, quantity: 0, quantityInStock: 350 },
-  {id: 1, name: 'Product 2', price: 3.19, quantity: 0, quantityInStock: 250 },
-  {id: 1, name: 'Product 3', price: 2.77, quantity: 0, quantityInStock: 150 },
-  {id: 1, name: 'Product 4', price: 4.80, quantity: 0, quantityInStock: 40 },
+  {id: 2, name: 'Product 2', price: 3.19, quantity: 0, quantityInStock: 250 },
+  {id: 3, name: 'Product 3', price: 2.77, quantity: 0, quantityInStock: 150 },
+  {id: 4, name: 'Product 4', price: 4.80, quantity: 0, quantityInStock: 40 },
 ]
 
 const Calculator:React.FC = () => {
