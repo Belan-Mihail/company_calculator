e6df991f0e780b8070d98c38c6ffe63fae1aff56
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-19 10:48:22 +0100

    add useEffect hook to DiscountInfo component. Condition order equal or more 1500

--- service-calculator-front/src/components/DiscountInfo.tsx
+++ service-calculator-front/src/components/DiscountInfo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Confetti from 'react-confetti/dist/types/Confetti'
 
 interface DiscountInfoProps {
@@ -9,6 +9,14 @@ const DiscountInfo: React.FC<DiscountInfoProps> = ({ total }) => {
     const [showConfetti, setShowConfetti] = useState(false);
     const [discountMessage, setDiscountMessage] = useState<string>('');
     const [textColor, setTextColor] = useState<string>('cream');
+
+    useEffect(() => {
+        if (total >= 1500) {
+            setDiscountMessage('You get 10% discount!')
+            setTextColor('green')
+            setShowConfetti(true)
+        }
+    })
 
   return (
     <div>DiscountInfo</div>
