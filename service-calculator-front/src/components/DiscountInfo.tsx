7590c33e7fad87952291ecffe97b259a9a32f09d
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-19 10:45:36 +0100

    add necessary states to DiscountInfo component

--- service-calculator-front/src/components/DiscountInfo.tsx
+++ service-calculator-front/src/components/DiscountInfo.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Confetti from 'react-confetti/dist/types/Confetti'
 
 interface DiscountInfoProps {
     total: number
 }
 
-const DiscountInfo = () => {
+const DiscountInfo: React.FC<DiscountInfoProps> = ({ total }) => {
+    const [showConfetti, setShowConfetti] = useState(false);
+    const [discountMessage, setDiscountMessage] = useState<string>('');
+    const [textColor, setTextColor] = useState<string>('cream');
+
   return (
     <div>DiscountInfo</div>
   )
