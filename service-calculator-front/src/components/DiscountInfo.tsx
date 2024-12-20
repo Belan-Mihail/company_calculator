94a0cc30d02e280288a77649a6c6471979f820f2
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-20 14:50:14 +0100

    clean up DiscountInfo.tsx

--- service-calculator-front/src/components/DiscountInfo.tsx
+++ service-calculator-front/src/components/DiscountInfo.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import Confetti from 'react-confetti'
 import '../styles/DiscountInfo.css'
 
 interface DiscountInfoProps {
@@ -7,9 +6,9 @@ interface DiscountInfoProps {
 }
 
 const DiscountInfo: React.FC<DiscountInfoProps> = ({ total }) => {
-    const [showConfetti, setShowConfetti] = useState(false);
+    
     const [discountMessage, setDiscountMessage] = useState<string>('');
-    const [textColor, setTextColor] = useState<string>('cream');
+    
     const [currentDiscount, setCurrentDiscount] = useState<number>(0)
     const [showEffect, setShowEffect] = useState(false)
 
@@ -34,16 +33,16 @@ const DiscountInfo: React.FC<DiscountInfoProps> = ({ total }) => {
 
         if (discount === 10) {
             setDiscountMessage('You get 10% discount!')
-            setTextColor('green')
+            
         } else if (discount === 7) {
             setDiscountMessage('You get 7% discount!')
-            setTextColor('green')
+            
         } else if (discount === 5) {
             setDiscountMessage('You get 5% discount!')
-            setTextColor('green')
+            
         } else {
             setDiscountMessage('Discount not available. Order more to get a discount')
-            setTextColor('cream')
+            
         }
 
         // Hide  after 5 seconds
@@ -62,7 +61,7 @@ const DiscountInfo: React.FC<DiscountInfoProps> = ({ total }) => {
         <p>7% discount on orders over $1,000</p>
         <p>5% discount on orders over $500</p>
         
-        <p style={{ color: textColor}} className={`text-xl ${showEffect ? 'glow-on-hover' : ''}`}>
+        <p className={`text-xl text-white ${showEffect ? 'glow-on-hover' : ''}`}>
             Current discount: {discountMessage}
         </p>
     </div>
