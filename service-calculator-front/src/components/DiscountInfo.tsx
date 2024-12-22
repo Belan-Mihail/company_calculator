ec5ccd90ce5d132b6a98497b01b3376fe077c918
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-22 22:00:35 +0100

    change DiscountInfo text color style

--- service-calculator-front/src/components/DiscountInfo.tsx
+++ service-calculator-front/src/components/DiscountInfo.tsx
@@ -60,20 +60,20 @@ const DiscountInfo: React.FC<DiscountInfoProps> = ({ total }) => {
     }, [total, showEffect, currentDiscount])
 
   return (
-    <div className='relative mb-8 p-4 rounded-xl bg-[#414a62] text-center flex flex-col gap-4'>
-        <div className='text-[#80d6b9]'>
-            <h2 className='text-[#fe8924]'>Discount Terms</h2>
-            <p>10% discount on orders over $1,500</p>
-            <p>7% discount on orders over $1,000</p>
-            <p>5% discount on orders over $500</p>
+    <div className='relative mb-8 p-4 rounded-xl text-center flex flex-col gap-4 text-xl'>
+        <div>
+            <h2 className='text-[#fe8924] text-4xl'>Discount Terms</h2>
+            <p className='text-blue-800'>10% discount on orders over $1,500</p>
+            <p className='text-blue-800'>7% discount on orders over $1,000</p>
+            <p className='text-blue-800'>5% discount on orders over $500</p>
         </div>
         
         
-        <p className={`text-xl text-[#ffea62] ${showEffect ? 'glow-on-hover' : ''}`}>
-            Current discount: {discountMessage}
+        <p className={`text-xl text-blue-800 ${showEffect ? 'glow-on-hover' : ''}`}>
+            {discountMessage}
         </p>
         {nextDiscountMessage && (
-                <p className="text-[#80d6b9] mt-4">{nextDiscountMessage}</p>
+                <p className="text-blue-800 mt-4">{nextDiscountMessage}</p>
             )}
     </div>
   )
