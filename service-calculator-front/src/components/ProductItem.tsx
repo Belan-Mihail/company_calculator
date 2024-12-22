9af9233a4919f9621ea30289f489cb6fe707b9d7
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-22 22:26:21 +0100

    delete unnecessary ProductItem.css file

--- service-calculator-front/src/components/ProductItem.tsx
+++ service-calculator-front/src/components/ProductItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ProductItemProps } from "../types/Product";
-import '../styles/ProductItem.css'
+
 
 const ProductItem: React.FC<ProductItemProps> = ({
   product,
