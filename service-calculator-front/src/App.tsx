a047a8390d5ed9d604603b652868c0c1919c88bb
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-22 21:51:10 +0100

    add styles for main class in App.css

--- service-calculator-front/src/App.tsx
+++ service-calculator-front/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   
 
   return (
-    <div className='flex flex-col items-center justify-center border-red-400 border-solid border-2 m-8 rounded '>
+    <div className='flex flex-col items-center justify-center border-red-400 border-solid border-2 m-8 rounded main '>
       <Calculator />
     </div>
   )
