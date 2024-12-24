5e678435d6552a4fa78a60ef76d70319ddcaeb99
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-24 14:54:13 +0100

    add logic to display Free delivery in Hamburg for orders over $50!

--- service-calculator-front/src/components/Total.tsx
+++ service-calculator-front/src/components/Total.tsx
@@ -8,7 +8,14 @@ interface TotalProps {
   finalTotal: number
 }
 
+
+
 const Total:React.FC<TotalProps> = ({total, totalWithDiscount, savings, deliveryCost, finalTotal}) => {
+
+  // Check if delivery cost is for Hamburg and total exceeds $50
+  const isHamburgDelivery = deliveryCost === 5 && total >= 50;
+  
+
   return (
     <div className='mt-2 text-base'>
       {savings > 0 ? (
@@ -16,7 +23,12 @@ const Total:React.FC<TotalProps> = ({total, totalWithDiscount, savings, delivery
         <p>Price without discount: {total.toFixed(2)} USD</p>
         <p>Price after discount: {totalWithDiscount.toFixed(2)} USD</p>
         <p>You saved: {savings.toFixed(2)} USD</p>
-        <p>Delivery Cost: {deliveryCost.toFixed(2)} USD</p>
+        {deliveryCost ? (<p>Delivery Cost: {deliveryCost.toFixed(2)} USD</p>) : ('')}
+        {isHamburgDelivery && (
+            <p>
+              Free delivery in Hamburg for orders over $50!
+            </p>
+          )}
         <p className="font-bold">Final Total: {finalTotal.toFixed(2)} USD</p>
         </>
       ) : (
@@ -25,12 +37,22 @@ const Total:React.FC<TotalProps> = ({total, totalWithDiscount, savings, delivery
             <>
             <p>Total price: {total.toFixed(2)} USD </p>
             {deliveryCost ? (<p>Delivery Cost: {deliveryCost.toFixed(2)} USD</p>) : ('')}
+            {isHamburgDelivery && (
+            <p>
+              Free delivery in Hamburg for orders over $50!
+            </p>
+          )}
             <p className="font-bold">Final Total: {finalTotal.toFixed(2)} USD</p>
             </>
           ) : (
             <>
             <p>Total price: {total.toFixed(2)} USD </p>
             {deliveryCost ? (<p>Delivery Cost: {deliveryCost.toFixed(2)} USD</p>) : ('')}
+            {isHamburgDelivery && (
+            <p>
+              Free delivery in Hamburg for orders over $50!
+            </p>
+          )}
             </>
           )}        
         </>
