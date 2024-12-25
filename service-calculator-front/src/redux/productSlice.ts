74110424d44063c002ebe5c8c96c0e261629fee7
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-25 07:40:47 +0100

    create reducer in productSlice

--- service-calculator-front/src/redux/productSlice.ts
+++ service-calculator-front/src/redux/productSlice.ts
@@ -19,4 +19,16 @@ const initialState: State = {
     error: ''
 }
 
+const reducer = (state: State, action: Action) => {
+    switch(action.type) {
+        case 'FETCH_REQUEST':
+            return {...state, loading: true}
+        case 'FETCH_SUCCESS':
+            return {...state, products: action.payload, loading: false}
+        case 'FETCH_FAIL':
+            return {...state, loading: false, error: action.payload}
+        default:
+            return state
+    }
+}
 
