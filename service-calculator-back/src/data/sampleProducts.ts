fd9542b3d92220ac2269bbe6c4892edd2ee06336
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-25 06:57:04 +0100

    import products in index.ts

--- service-calculator-back/src/data/sampleProducts.ts
+++ service-calculator-back/src/data/sampleProducts.ts
@@ -6,7 +6,7 @@ export type Product = {
     quantityInStock: number,
 }
 
-const products: Product[] = [
+export const products: Product[] = [
     {id: 1, name: 'Product 1', price: 2.34, quantity: 0, quantityInStock: 350 },
     {id: 2, name: 'Product 2', price: 3.19, quantity: 0, quantityInStock: 250 },
     {id: 3, name: 'Product 3', price: 2.77, quantity: 0, quantityInStock: 150 },
--- service-calculator-back/src/index.ts
+++ service-calculator-back/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { products } from './data/sampleProducts';
 
 const app = express();
 
