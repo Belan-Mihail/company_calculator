eba7eb318a71b922529710cc1eed7f90fb2fc779
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-25 08:21:52 +0100

    modify store in frontend

--- service-calculator-front/src/redux/store.ts
+++ service-calculator-front/src/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import calculatorReducer from './calculatorSlice';
+import {reducer} from './productReducer'
 
 export const store = configureStore({
     reducer: {
         calculator: calculatorReducer,
+        products: reducer
     },
 })
