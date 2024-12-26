f158aa5beae4a8977c2ccc1d54548cbf3deac23d
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-26 13:45:53 +0100

    create ServiceProductModel

--- service-calculator-back/src/models/Product.ts
+++ service-calculator-back/src/models/Product.ts
@@ -1,8 +1,21 @@
 import mongoose, {Document, Schema } from "mongoose";
 
-interface Product extends Document {
-    name: string,
-    price: number,
-    quantity: number,
-    quantityInStock: number,
+interface ServiceProduct extends Document {
+    product_name: string,
+    product_price: number,
+    product_quantity: number,
+    product_quantityInStock: number,
 }
+
+const productSchema: Schema = new Schema ({
+    product_name: {type: String, required: true, unique: true},
+    product_price: {type: Number, required: true},
+    product_quantity: {type: Number, required: true, min: 0},
+    product_quantityInStock: {type: Number, required: true, min: 0},
+},
+{ timestamps: true }
+)
+
+const ServiceProductModel = mongoose.model<ServiceProduct>('ServiceProduct', productSchema)
+
+export default ServiceProductModel;
