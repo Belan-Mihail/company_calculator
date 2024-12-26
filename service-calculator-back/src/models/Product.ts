8d15e6a1058c0430fe7b7ab745184f8654fc0259
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-26 14:06:10 +0100

    add Middleware to set product_quantity to 0 always

--- service-calculator-back/src/models/Product.ts
+++ service-calculator-back/src/models/Product.ts
@@ -16,6 +16,12 @@ const serviceProductSchema: Schema = new Schema ({
 { timestamps: true }
 )
 
+// Middleware to set product_quantity to 0 always
+serviceProductSchema.pre('save', function(next) {
+    this.product_quantity = 0;
+    next()
+})
+
 const ServiceProductModel = mongoose.model<ServiceProduct>('ServiceProduct', serviceProductSchema)
 
 export default ServiceProductModel;
