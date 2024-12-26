301d23d7d2c6ebf06637cf9acdbad11f9e456cea
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-26 14:02:08 +0100

    create createProduct controller

--- service-calculator-back/src/controllers/productController.ts
+++ service-calculator-back/src/controllers/productController.ts
@@ -2,4 +2,19 @@ import { Request, Response } from 'express';
 import ServiceProductModel from '../models/Product';
 
 // Create a new product
-export const createProduct
+export const createProduct = async (req: Request, res: Response):Promise<void> => {
+    try {
+        const {product_name, product_price, product_quantity, product_quantityInStock} = req.body;
+        const newProduct = new ServiceProductModel({
+            product_name,
+            product_price,
+            product_quantity,
+            product_quantityInStock,
+        });
+        await newProduct.save();
+        res.status(201).json(newProduct)
+    } catch (error) {
+        res.status(400).json({ message: error.message})
+    }
+
+}
