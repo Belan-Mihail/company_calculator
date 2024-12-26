bddd60a43f669a63ce2500b0aa6d87edaa83888e
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-26 14:26:04 +0100

    create updateProduct controller

--- service-calculator-back/src/controllers/productController.ts
+++ service-calculator-back/src/controllers/productController.ts
@@ -1,44 +1,89 @@
-import { Request, Response } from 'express';
-import ServiceProductModel from '../models/Product';
+import { Request, Response } from "express";
+import ServiceProductModel from "../models/Product";
 
 // Create a new product
-export const createProduct = async (req: Request, res: Response):Promise<void> => {
-    try {
-        const {product_name, product_price, product_quantity, product_quantityInStock} = req.body;
-        const newProduct = new ServiceProductModel({
-            product_name,
-            product_price,
-            product_quantity,
-            product_quantityInStock,
-        });
-        await newProduct.save();
-        res.status(201).json(newProduct)
-    } catch (error) {
-        res.status(400).json({ message: error.message})
-    }
-}
+export const createProduct = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const {
+      product_name,
+      product_price,
+      product_quantity,
+      product_quantityInStock,
+    } = req.body;
+    const newProduct = new ServiceProductModel({
+      product_name,
+      product_price,
+      product_quantity,
+      product_quantityInStock,
+    });
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 // get all products
-export const getAllProducts = async (req:Request, res: Response):Promise<void> => {
-    try {
-        const products = await ServiceProductModel.find()
-        res.json(products)
-    } catch (error) {
-        res.status(400).json({message: error.message})
-    }
-}
+export const getAllProducts = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const products = await ServiceProductModel.find();
+    res.json(products);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 // get product by ID
-export const getProductById = async (req:Request, res: Response): Promise<void> => {
-    try {
-        const product = await ServiceProductModel.findById(req.params.id);
-        if (!product) {
-            res.status(400).json({ message: 'Product not found'})
-            return
-        }
-        res.json(product)
-    } catch (error) {
-        res.status(400).json({message: error.message})
+export const getProductById = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const product = await ServiceProductModel.findById(req.params.id);
+    if (!product) {
+      res.status(400).json({ message: "Product not found" });
+      return;
     }
-}
+    res.json(product);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
+// Update Product
+export const UpdateProduct = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const {
+      product_name,
+      product_price,
+      product_quantity,
+      product_quantityInStock,
+    } = req.body;
+    const product = await ServiceProductModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        product_name,
+        product_price,
+        product_quantity,
+        product_quantityInStock,
+      },
+      { new: true }
+    );
+    if (!product) {
+        res.status(400).json({ message: 'Product not found'})
+        return
+    }
+    res.json(product)
+  } catch (error) {
+    res.status(400).json({ message: error.message})
+  }
+};
