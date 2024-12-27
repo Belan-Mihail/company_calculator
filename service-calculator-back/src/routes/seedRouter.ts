8a332834bc71a264bba8c82980b77e6bdaccaa8a
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-27 16:25:20 +0100

    edit seedRouter code

--- service-calculator-back/src/routes/seedRouter.ts
+++ service-calculator-back/src/routes/seedRouter.ts
@@ -1,4 +1,20 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, Router } from 'express';
 import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel';  // 
 import { sampleProducts } from '../data/sampleProducts'; 
+
+const seedRouter = express.Router()
+
+seedRouter.get('products/seed', asyncHandler(async (req: Request, res: Response) => {
+    try {
+        await Product.deleteMany({})
+
+        const createdProducts = await Product.insertMany(sampleProducts)
+
+        res.json({createdProducts})
+    } catch (error) {
+        res.status(500).json({ message: error.message})
+    }
+}))
+
+export default seedRouter
