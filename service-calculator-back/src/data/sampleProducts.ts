5968816be4d17c259bcb8c8e91008d3edbc31749
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-27 16:15:06 +0100

    edit test product array in sampleProducts.ts

--- service-calculator-back/src/data/sampleProducts.ts
+++ service-calculator-back/src/data/sampleProducts.ts
@@ -7,8 +7,8 @@ export type Product = {
 }
 
 export const products: Product[] = [
-    {id: 1, name: 'Product 1', price: 2.34, quantity: 0, quantityInStock: 350 },
-    {id: 2, name: 'Product 2', price: 3.19, quantity: 0, quantityInStock: 250 },
-    {id: 3, name: 'Product 3', price: 2.77, quantity: 0, quantityInStock: 150 },
-    {id: 4, name: 'Product 4', price: 4.80, quantity: 0, quantityInStock: 40 },
+    {product_name: 'Product 1', product_price: 3.34, product_quantity: 0, product_quantityInStock: 350 },
+    {product_name: 'Product 2', product_price: 4.19, product_quantity: 0, product_quantityInStock: 250 },
+    {product_name: 'Product 3', product_price: 3.77, product_quantity: 0, product_quantityInStock: 150 },
+    {product_name: 'Product 4', product_price: 5.80, product_quantity: 0, product_quantityInStock: 40 },
   ]
