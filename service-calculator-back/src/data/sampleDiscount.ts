cd604ce84af2083da01dd9f7215c04f19397ef0d
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-28 13:43:49 +0100

    create discounts const in sampleDiscounts.ts

--- service-calculator-back/src/data/sampleDiscount.ts
+++ service-calculator-back/src/data/sampleDiscount.ts
@@ -1,4 +1,10 @@
-export type Discount {
+export type Discount = {
     discount_size: number
     available_from: number
 }
+
+export const discounts:Discount[] = [
+    {discount_size: 5, available_from: 500 },
+    {discount_size: 7, available_from: 1000 },
+    {discount_size: 10, available_from: 1500 }
+]
