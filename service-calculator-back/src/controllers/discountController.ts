3031e0aa2bbdc3e9aa10f3398ca1754451668a6d
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-28 13:57:51 +0100

    create createDiscount controller

---
+++ service-calculator-back/src/controllers/discountController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import ServiDiscountModel from "../models/Discount";
+
+// create a new Discount
+export const createDiscount = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const {discount_size, available_from} = req.body
+        const newDiscount = new ServiDiscountModel({
+            discount_size, 
+            available_from,
+        })
+        await newDiscount.save()
+        res.status(200).json(newDiscount)
+    } catch (error) {
+        res.status(400).json({ message: error.message})        
+    }
+}
