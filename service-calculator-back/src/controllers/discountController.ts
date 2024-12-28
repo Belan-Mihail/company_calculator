c36f34b4f735bfca89855fe4f6ea8312b1713d17
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-28 14:18:50 +0100

    create deleteDiscount controller

--- service-calculator-back/src/controllers/discountController.ts
+++ service-calculator-back/src/controllers/discountController.ts
@@ -47,10 +47,25 @@ export const updateDicount = async (req: Request, res: Response):Promise<void> =
         const discount = await ServisDiscountModel.findByIdAndUpdate(req.params.id, {discount_size, available_from}, {new: true})
         if (!discount) {
             res.status(400).json({message: 'Discount not found'})
+            return
         }
         res.json(discount)
     } catch (error) {
         res.status(400).json({ message: error.message})
         
+    }
+}
+
+// delete Discount
+export const deleteDiscount = async (req: Request, res: Response):Promise<void> => {
+    try {
+        const discount = await ServisDiscountModel.findByIdAndDelete(req.params.id)
+        if (!discount) {
+            res.status(400).json({message: 'Discount not found'})
+            return
+        }
+        res.json({message: 'The discount has been successfully deleted.'})
+    } catch (error) {
+        res.status(400).json({message: error.message})
     }
 }
