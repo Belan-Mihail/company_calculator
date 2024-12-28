8458f2d6f847f4737f2ad23902ff0b3936c996f8
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-28 22:00:51 +0100

    finish with discountRoutes.ts

--- service-calculator-back/src/routes/discountRoutes.ts
+++ service-calculator-back/src/routes/discountRoutes.ts
@@ -1,2 +1,12 @@
 import { Router } from 'express';
 import { createDiscount, getAllDiscounts, getDiscountById, updateDicount, deleteDiscount } from '../controllers/discountController';
+
+const router:Router = Router()
+
+router.post('/discounts', createDiscount)
+router.get('/discounts', getAllDiscounts)
+router.get('/discounts/:id', getDiscountById)
+router.put('/discounts/id', updateDicount)
+router.delete('/discounts/:id', deleteDiscount)
+
+export default router
