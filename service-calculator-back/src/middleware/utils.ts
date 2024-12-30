ddb0d4697eb4b030262aa54744cae3cfa1596bd8
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-30 22:33:42 +0100

    add code to utils.ts

--- service-calculator-back/src/middleware/utils.ts
+++ service-calculator-back/src/middleware/utils.ts
@@ -1,2 +1,17 @@
 import jwt from 'jsonwebtoken'
 import { Request, Response, NextFunction } from 'express'
+
+const authenticAdmin = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.header('Autorization')?.replace('Bearer ', '')
+    if (!token) {
+        return res.status(401).json({message: 'No token provided'})
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'somethingsecret')
+        req.user = decoded
+        next()
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token' })
+    }
+}
