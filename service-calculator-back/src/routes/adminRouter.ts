b453e7aa4edc928a09a06ca1ee9104425003dd63
Bilan Mykhailo <bilanmo1488@gmail.com>
2024-12-30 21:49:48 +0100

    create admin Router code

---
+++ service-calculator-back/src/routes/adminRouter.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response, Router } from 'express';
+import asyncHandler from 'express-async-handler';
+import AdminModel from '../models/Admin';
+
+const adminRouter = express.Router()
+
+adminRouter.get('/create-admin', asyncHandler(async (req: Request, res: Response) => {
+    try {
+        const username = ''
+        const password = ''
+
+        // create a newAdmin 
+        const newAdmin = new AdminModel({username, password})
+        await newAdmin.save()
+        res.status(201).json({message: 'Admin created succesfully!'})
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}))
