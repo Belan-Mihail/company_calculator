99b9657c4f2fcf19bc76492462b8cbe8735aa6a0
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-01 12:29:21 +0100

    import authenticAdmin in discountRouters and add it to routers

--- service-calculator-back/src/routes/discountRoutes.ts
+++ service-calculator-back/src/routes/discountRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { createDiscount, getAllDiscounts, getDiscountById, updateDicount, deleteDiscount } from '../controllers/discountController';
+import { authenticAdmin } from '../middleware/utils'; 
 
 const router:Router = Router()
 
-router.post('/discounts', createDiscount)
+router.post('/discounts', authenticAdmin, createDiscount)
 router.get('/discounts', getAllDiscounts)
-router.get('/discounts/:id', getDiscountById)
-router.put('/discounts/id', updateDicount)
-router.delete('/discounts/:id', deleteDiscount)
+router.get('/discounts/:id', authenticAdmin, getDiscountById)
+router.put('/discounts/id', authenticAdmin, updateDicount)
+router.delete('/discounts/:id', authenticAdmin, deleteDiscount)
 
 export default router
