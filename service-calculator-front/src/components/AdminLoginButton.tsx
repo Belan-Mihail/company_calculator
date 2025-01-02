236da51e931b1357fe712fc6da4da06edff2b34e
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-02 08:34:57 +0100

    fix bug- use navigate to redirect user to login page

--- service-calculator-front/src/components/AdminLoginButton.tsx
+++ service-calculator-front/src/components/AdminLoginButton.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const AdminLoginButton:React.FC = () => {
-    const history = useHistory()
+    const navigate = useNavigate()
 
     const handleClick = () => {
-        history.push('/login')
+        navigate('/login')
     }
 
   return (
