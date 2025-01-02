17dd91dc484443aedf23e1eadb7400982b6e146d
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-02 08:56:34 +0100

    add handleSubmit function to LoginPage

--- service-calculator-front/src/pages/LoginPage.tsx
+++ service-calculator-front/src/pages/LoginPage.tsx
@@ -4,8 +4,32 @@ import { useNavigate } from 'react-router-dom'
 const LoginPage = () => {
     const [username, setUsername] = useState<string>('')
     const [password, setPassword] = useState<string>('')
-    const [error, setErorr] = useState<string>('')
+    const [error, setErorr] = useState('')
     const navigate = useNavigate()
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:3000/api/admin/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({username, password})
+            })
+
+            const data = await response.json()
+
+            if (!response.ok) {
+                throw new Error(data.message || 'Something went wrong')
+            }
+
+            localStorage.setItem('token', data.token)
+            navigate('/dashboard')
+        } catch (error: any) {
+            setErorr(error.message || 'Something went wrong')
+        }
+    }
 
 
   return (
