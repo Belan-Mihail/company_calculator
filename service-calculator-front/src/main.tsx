ca8a2111ed6ab1e9dab14945ae0d8cc2242c4491
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-02 08:18:55 +0100

    add router provider to main.tsx

--- service-calculator-front/src/main.tsx
+++ service-calculator-front/src/main.tsx
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>
     
   
