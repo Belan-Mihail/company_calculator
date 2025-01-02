6e467ae58f45b8d18a0d158ee9287e60eaa92fe7
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-02 08:48:05 +0100

    create necessary states in LoginPage

--- service-calculator-front/src/pages/LoginPage.tsx
+++ service-calculator-front/src/pages/LoginPage.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const LoginPage = () => {
+    const [username, setUsername] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [error, setErorr] = useState<string>('')
+    const navigate = useNavigate()
+
+
   return (
     <div>LoginPage</div>
   )
