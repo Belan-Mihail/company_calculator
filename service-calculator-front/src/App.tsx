924272329552c56f064b55445d735ff52987ea78
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-02 08:39:12 +0100

    edit JSX on App.tsx to show Login as Admin button

--- service-calculator-front/src/App.tsx
+++ service-calculator-front/src/App.tsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import './index.css'
 import './App.css'
 import Calculator from './components/Calculator'
+import AdminLoginButton from './components/AdminLoginButton'
 
 function App() {
   
 
   return (
-    <div className='flex flex-col items-center justify-center m-8 rounded main mx-auto mt-10 w-max'>
-      <Calculator />
+    <div className='flex flex-col items-center justify-center'>
+      <div className='flex flex-col items-center justify-center m-8 rounded main mx-auto mt-10 w-max'>
+        <Calculator />
+        
+      </div>
+      <AdminLoginButton />
     </div>
+    
   )
 }
 
