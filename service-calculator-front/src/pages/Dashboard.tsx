749bdf15211d8cf9f3fe782288e287e45bd13f2b
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-07 10:25:47 +0100

    import useNavigate in Daschboard page and define navigate const

--- service-calculator-front/src/pages/Dashboard.tsx
+++ service-calculator-front/src/pages/Dashboard.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react'import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+
 
 const Dashboard = () => {
+  const navigate = useNavigate()
+
+  
   return (
     <div>Dashboard</div>
   )
