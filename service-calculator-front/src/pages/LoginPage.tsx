4b360b1a0ea973931fd40f08bcc5305eb237c588
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-07 10:56:21 +0100

    add logic to loginpage to show error with roastify

--- service-calculator-front/src/pages/LoginPage.tsx
+++ service-calculator-front/src/pages/LoginPage.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const LoginPage = () => {
     const [username, setUsername] = useState<string>('')
     const [password, setPassword] = useState<string>('')
     const [error, setErorr] = useState('')
     const navigate = useNavigate()
+
+     
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -21,7 +25,9 @@ const LoginPage = () => {
             const data = await response.json()
 
             if (!response.ok) {
-                throw new Error(data.message || 'Something went wrong')
+                // show error message
+                toast.error(data.message || 'Something went wrong')
+                
             }
 
             localStorage.setItem('token', data.token)
