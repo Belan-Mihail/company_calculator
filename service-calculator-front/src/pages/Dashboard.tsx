bb381995fe6330d29789205d26f237186fba028f
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-08 09:02:38 +0100

    create product and discount states on DashboardPage

--- service-calculator-front/src/pages/Dashboard.tsx
+++ service-calculator-front/src/pages/Dashboard.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-
-
-
+import { Product } from '../types/Product'
+import { Discount } from '../types/Discount'
 
 const Dashboard = () => {
   const navigate = useNavigate()
+
+//  states for products and discounts
+const [product, setProduct] = useState<Product[]>([])
+const [discount, setDiscount] = useState<Discount[]>([])
 
   const handleReturnToMainPage = () => {
     navigate('/')
