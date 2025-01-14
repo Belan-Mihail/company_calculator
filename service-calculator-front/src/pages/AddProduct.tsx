976275630f2f19d5de5bd1a471e19c2014b0cb2c
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-14 09:53:01 +0100

    add needly import on AddProduct Page

--- service-calculator-front/src/pages/AddProduct.tsx
+++ service-calculator-front/src/pages/AddProduct.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AddProduct = () => {
+
+    
+
   return (
     <div>AddProduct</div>
   )
