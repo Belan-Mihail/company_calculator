f8d3c011b83c7c4525633cc0f878ec2e9dcbc196
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-16 09:12:26 +0100

    create DashboardButton component

---
+++ service-calculator-front/src/components/DashboardButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardButton = () => {
+  return (
+    <div>DashboardButton</div>
+  )
+}
+
+export default DashboardButton
--- service-calculator-front/src/pages/HomePage.tsx
+++ service-calculator-front/src/pages/HomePage.tsx
@@ -5,12 +5,15 @@ import '../index.css'
 import '../App.css'
 
 const HomePage:React.FC = () => {
+    
+
   return (
     <div className='flex flex-col items-center justify-center'>
       <div className='flex flex-col items-center justify-center m-8 rounded main mx-auto mt-10 w-max'>
         <Calculator />
         
       </div>
+      
       <AdminLoginButton />
     </div>
   )
