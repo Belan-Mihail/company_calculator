52cd8b34d0ac11d2d648ef8e683cb894b36f302e
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-16 09:33:32 +0100

    create handleLogout function in LogoutButton component and add code to catch block

--- service-calculator-front/src/components/LogoutButton.tsx
+++ service-calculator-front/src/components/LogoutButton.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const LogoutButton = () => {
+const LogoutButton:React.FC = () => {
+    const navigate = useNavigate()
+
+    const handleLogout = async () => {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
   return (
     <div>LogoutButton</div>
   )
