a8631fec9ab3af3ab003e69832a62b8d747b5fce
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-16 09:40:16 +0100

    finish with handleLogout function in Logout component

--- service-calculator-front/src/components/LogoutButton.tsx
+++ service-calculator-front/src/components/LogoutButton.tsx
@@ -6,7 +6,23 @@ const LogoutButton:React.FC = () => {
 
     const handleLogout = async () => {
         try {
-            
+            // Make a request to the backend to log the user out
+            const response = await fetch('http://localhost:3000/api/admin/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+
+            if (response.ok) {
+                 // If logout is successful, clear the local token and redirect to login page
+                 localStorage.removeItem('token')
+                 navigate('/')
+            } else {
+                const data = await response.json()
+                console.log('Logout failed: ', data.message)
+            }
         } catch (error) {
             console.log(error)
         }
