cc732062d9e817ceb464ea955bff92d143591ac9
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-16 09:44:57 +0100

    edit App tsx. add logout button

--- service-calculator-front/src/App.tsx
+++ service-calculator-front/src/App.tsx
@@ -1,10 +1,19 @@
 
-function App() {
-  
+import React from 'react'
+import LogoutButton from './components/LogoutButton'
+
+const App: React.FC = () => {
+  const token = localStorage.getItem('token')
 
   return (
-    <></>
-    
+    <div>
+      {token && (
+        <div className=' fixed top-2.5 right-2.5'>
+          <LogoutButton />
+        </div>
+        
+      )}
+    </div>
   )
 }
 
