385ea4e0d06ee60eea051b5ffbf104577febda94
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-17 15:31:58 +0100

    create confirmlogoutModulProps interface

--- service-calculator-front/src/components/ConfirmLogoutModal.tsx
+++ service-calculator-front/src/components/ConfirmLogoutModal.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+interface ConfirmLogoutModulProps {
+    onConfirm: () => void
+    onCancel: () => void
+}
+
 const ConfirmLogoutModal = () => {
   return (
     <div>ConfirmLogoutModal</div>
