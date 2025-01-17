6bb90e29b559ce5670ed0f0bb4c9e57de2b21d13
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-17 09:44:11 +0100

    edit App.tsx with useAuth

--- service-calculator-front/src/App.tsx
+++ service-calculator-front/src/App.tsx
@@ -1,26 +1,11 @@
 
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import LogoutButton from './components/LogoutButton'
 import { Outlet } from 'react-router-dom'
+import { useAuth } from './hooks/AuthContext';
 
 const App: React.FC = () => {
-  const [token, setToken] = useState<string | null>(localStorage.getItem('token'))
-
-  // Check for the token on initial mount and whenever the localStorage changes
-  useEffect(() => {
-    
-    const handleStorageChange = () => {
-      setToken(localStorage.getItem('token'))
-    }
-
-    window.addEventListener('storage', handleStorageChange)
-
-    // clean Listener
-    return () => {
-      window.removeEventListener('storage', handleStorageChange)
-    }
-
-  }, [])
+  const { token } = useAuth();
 
   return (
     <div>
