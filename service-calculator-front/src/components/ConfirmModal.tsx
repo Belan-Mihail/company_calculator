bfbc85b00724f8dc42d8f6f4bce92d91700389e4
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-18 13:38:17 +0100

    edit confirm button header text a

--- service-calculator-front/src/components/ConfirmModal.tsx
+++ service-calculator-front/src/components/ConfirmModal.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 interface ConfirmModulProps {
     onConfirm: () => void
     onCancel: () => void
+    headerText: string
 }
 
-const ConfirmModal: React.FC<ConfirmModulProps> = ({onConfirm, onCancel}) => {
+const ConfirmModal: React.FC<ConfirmModulProps> = ({onConfirm, onCancel, headerText}) => {
    
   return (
     <div className='fixed main z-50 w-80 h-80 left-1/2 transform -translate-x-1/2 top-16 p-4 flex justify-center items-center'>
         <div className='round flex flex-col justify-center items-center gap-4'>
-            <h2 className='text-xl mb-2'>Are you sure you want to exit?</h2>
+            <h2 className='text-xl mb-2'>{headerText}</h2>
             <div className='flex gap-4'>
                 <button className='main-button' onClick={onConfirm}>Confirm</button>
                 <button className='main-button' onClick={onCancel}>Cancel</button>
