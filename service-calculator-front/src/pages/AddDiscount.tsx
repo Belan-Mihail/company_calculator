0d2df31707bcf7e1090eaf77dd3257c0e291a9fa
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-19 20:08:53 +0100

    create DiscountFormData interface

--- service-calculator-front/src/pages/AddDiscount.tsx
+++ service-calculator-front/src/pages/AddDiscount.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+interface DiscountFormData {
+    discount_size: string
+    discount_availible_from: string
+}
+
 const AddDiscount = () => {
   return (
     <div>AddDiscount</div>
