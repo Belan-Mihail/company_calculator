54795830b7ea243c4368ad261b9ff1cac899d739
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-23 09:41:55 +0100

    create ValidationError interface and state to manage validation error message

--- service-calculator-front/src/pages/EditDiscountPage.tsx
+++ service-calculator-front/src/pages/EditDiscountPage.tsx
@@ -4,6 +4,11 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { Discount } from '../types/Discount'
 import {toast, toastContainer } from 'react-toastify'
 
+interface ValidationErrors {
+  discountSizeError: string | null
+  discountAvailableFromError: string | null
+}
+
 
 const EditDiscountPage = () => {
   const {token} = useAuth()
@@ -14,6 +19,12 @@ const EditDiscountPage = () => {
   const [discountData, setDiscountData] = useState<Discount>({
     discount_size: 0,
     available_from: 0
+  })
+
+  // state to manage validation error messages
+  const [validationError, setValidationErrors] = useState<ValidationErrors>({
+    discountSizeError: null,
+    discountAvailableFromError: null
   })
 
   return (
