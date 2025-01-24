321d292e3f78d89e70bb9465dcd218bc636c509d
Bilan Mykhailo <bilanmo1488@gmail.com>
2025-01-24 09:20:36 +0100

    add handleClick function to ReturnToMainButton component

--- service-calculator-front/src/components/ReturnToMainButton.tsx
+++ service-calculator-front/src/components/ReturnToMainButton.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 const ReturnToMainButton = () => {
+
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        navigate('/')
+    }
+
   return (
     <div>ReturnToMainButton</div>
   )
